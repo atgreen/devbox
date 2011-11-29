9d17a66fe162c634c82af09453a1a8f27c86e34c
Anthony Green <green@moxielogic.com>
2011-11-29 17:41:27 -0500

    Test output

--- devbox-setup.sh
+++ devbox-setup.sh
@@ -1,32 +1,42 @@
 #!/bin/sh
 
+echo AAAA
+
 # Make sure that we are root
 if [ `whoami` != 'root' ]; then
   echo "permission denied. (sudo su -)"
   exit
 fi
 
+echo BBBB
 # This is me
 USER=green
 
 # Double check that we're fully up-to-date
 yum -y update
+echo CCCC
 
 # Create my user
 /usr/sbin/useradd $USER
+echo DDDD
 
 # Make me all powerful
 /bin/echo '$USER  ALL=(ALL)  NOPASSWD: ALL' >> /etc/sudoers
 
+echo EEEE
 # Download the mighty emacs config
 sudo su - $USER -c "git clone git://github.com/atgreen/emacs && mv emacs .emacs.d && rm .emacs && touch .emacs.d/private.el"
 
+echo FFFF
 # Install the proprietary but nevertheless useful dropbox
 sudo su - $USER -c "wget -O - http://www.dropbox.com/download?plat=lnx.x86_64 | tar xzf -"
 
+echo GGGG
 # Install quicklisp
 sudo su - $USER -c "wget http://beta.quicklisp.org/quicklisp.lisp"
+echo HHHH
 sudo su - $USER -c "sbcl --load quicklisp.lisp --eval '(progn (quicklisp-quickstart:install) (sb-ext:quit))'"
+echo IIII
 
 
 
